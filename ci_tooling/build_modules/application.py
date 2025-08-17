124d1fe6dec178afa93f3a1137541bcf4096e04a
Code Jumping <fairzlq@hotmail.com>
2025-08-17 21:07:19 +0800

    Update icons

--- ci_tooling/build_modules/application.py
+++ ci_tooling/build_modules/application.py
@@ -146,6 +146,7 @@ class GenerateApplication:
         print("Patching LC_BUILD_VERSION")
         with open(_file, "rb") as f:
             data = f.read()
+            
             data = data.replace(_find, _replace)
 
         with open(_file, "wb") as f:
