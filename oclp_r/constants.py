21853ea65e383f5f4c0c0cd900f0cf1c6dd58f74
Ghltbm <138438187+pyquick@users.noreply.github.com>
2026-09-13 13:52:20 +0800

    update upload.

--- oclp_r/constants.py
+++ oclp_r/constants.py
@@ -23,7 +23,7 @@ class Constants:
 
         # Patcher Versioning
         self.patcher_version:                 str = "3.1.9"  # OCLP-R
-        self.patcher_support_pkg_version:     str = "1.11.7"  # PatcherSupportPkg
+        self.patcher_support_pkg_version:     str = "1.11.8"  # PatcherSupportPkg
         self.copyright_date:                  str = "Copyright © 2020-2026 Dortania and Hackdoc"
         self.patcher_name:                    str = "OCLP-R"
         
